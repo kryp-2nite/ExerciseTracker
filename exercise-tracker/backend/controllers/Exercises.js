a2f8cc0ed4ea12e7bebcc37a0b68adb81ab05d81
Jonathan Flores <jonathanflores122@hotmail.com>
2021-11-04 19:36:05 -0700

    finished controllers

--- exercise-tracker/backend/controllers/Exercises.js
+++ exercise-tracker/backend/controllers/Exercises.js
@@ -58,11 +58,99 @@ const showExercisePostById = (req, res) => {
 };
 
 
+const updateExercise = (req, res) => {
+    db.ExercisePost.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true },
+        (err, updatedPost) => {
+            if (err) console.log("Error in Post#update: ", err);
+
+            return res.status(202).json({
+                data: updatedPost,
+            });
+        }
+    );
+};
 
 
 
 
 
+
+const showComments = (req, res) => {
+    ExercisePost.findById(req.params.id)
+    .then(foundPost => {
+        if (!foundPost) return console.log("Error in Comment#Show")
+        return res.status(200).json({ 
+            message: "Success",
+            data: foundPost.comments,
+        });
+    })
+    .catch(err => console.log(err))
+};
+
+const createComment = (req, res) => {
+    ExercisePost.findById(req.params.id)
+    .then(foundPost => {
+        if(!foundPost) return console.log("Error in Comment#Create: ")
+        foundPost.comments.push(req.body);
+        foundPost.save();
+
+        return res.status(201).json({ 
+            message: "Succes",
+            data: foundPost.comments,
+        });
+    })
+    .catch(err => console.log(err))
+};
+
+const updateComment = (req, res) => {
+    ExercisePost.findById(req.params.id)
+    .then(foundPost => {
+        if (!foundPost) return console.log("Error in Comment#update")
+
+        const commentById = foundPost.comments.id(req.params.commentId);
+        commentById.author = req.body.author;
+        commentById.content = req.body.content;
+        foundPost.save();
+
+        return res.status(202).json({ 
+            message: "Success",
+            data: commentById,
+        });
+    })
+};
+
+
+
+const destroy = (req, res) => {
+    ExercisePost.findByIdAndDelete(req.params.id, (err, deletedPost) => {
+        if (err) console.log("Error in Post#destroy:  ", err);
+
+        return res.status(200).json({
+            data: deletedPost,
+        })
+    })
+}
+
+
+const destroyComment = (req, res) => {
+    ExercisePost.findById(req.params.id)
+    .then(foundPost => {
+        if (!foundPost) return console.log("Error in Comment#")
+
+        const commentById = foundPost.comments.id(req.params.commentId)
+        console.log(commentById)
+        commentById.remove();
+        foundPost.save();
+
+        return res.status(200).json({
+            message: "success",
+            data: commentById,
+        })
+    })
+}
 
 
 
@@ -70,5 +158,10 @@ module.exports = {
     createExercisePost,
     index,
     showExercisePostById,
-    
+    showComments,
+    createComment,
+    updateComment,
+    destroy,
+    destroyComment,
+    updateExercise
 }
