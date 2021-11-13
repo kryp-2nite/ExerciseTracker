6a6fd9542b6491c937ee3450fc71cffa52d293ac
Jonathan Flores <jonathanflores122@hotmail.com>
2021-11-13 10:07:23 -0800

    added some backend side

--- exercise-tracker/backend/server.js
+++ exercise-tracker/backend/server.js
@@ -10,9 +10,26 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, '..', 'build')));
+app.use(express.static('public'));
 app.use(express.json());
 
+
+//custom logger to show the url and req.body if one exists
+app.use((req, res, next) => {
+    console.log(req.url);
+    if (req.body) {
+        console.log("BODY BEING SENT: ", req.body);
+    }
+    next();
+});
+
 //need /api routes here later one!!!
+app.use("/api", routes);
+
+app.all("/api/*", function (req, res, next) {
+    res.send("THIS IS NOT AN API ROUTE");
+});
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('exercise-tracker/build'));
