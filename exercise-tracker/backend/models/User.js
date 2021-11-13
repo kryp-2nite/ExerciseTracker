e59de5532cdea7aa9f6beb1ba43ab0294e7dfcba
Jonathan Flores <jonathanflores122@hotmail.com>
2021-11-13 14:10:50 -0800

    added log out plus links

--- exercise-tracker/backend/models/User.js
+++ exercise-tracker/backend/models/User.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema(
         firstName: { type: String, required: true, minlength: 2, maxlength: 50 },
         lastName: { type: String, required: true, minlength: 2, maxlength: 50},
         email: { type: String, required: true, unique: true },
-        password: { type: String, required: true, minlength: 5 },
+        password: { type: String, required: true, minlength: 3 },
     },
     {
         timestamps: true,
@@ -17,7 +17,9 @@ const userSchema = new mongoose.Schema(
 )
 
 userSchema.pre('save', function (next) {
-    const user = this.getUser();if(!user.isModified('password')) return next();
+
+    const user = this;
+    if(!user.isModified('password')) return next();
     bcrypt.hash(user.password, SALT_ROUNDS, function (err, hash) {
         if (err) return next(err);
         user.password = hash;
